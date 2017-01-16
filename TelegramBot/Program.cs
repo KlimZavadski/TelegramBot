e175691c0d99c3e131a892ac62b823922e8c2a4e
Klim Zavadski <klimka911@gmail.com>
2017-01-16 21:26:59 +0300

    Initial commit.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
